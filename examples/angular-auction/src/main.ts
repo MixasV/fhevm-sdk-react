e5babd4e8b18251ab11f7603741ee0c9a468d984
MixasV <m.varich@yandex.ru>
2025-10-31 19:45:17 +0300

    fix: resolve Angular JIT compiler error in production build

--- examples/angular-auction/src/main.ts
+++ examples/angular-auction/src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser'
+import { importProvidersFrom } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app/app.component'
+import { FHEVMService } from '@mixaspro/angular'
 
-bootstrapApplication(AppComponent)
-  .catch(err => console.error(err))
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(BrowserModule),
+    FHEVMService
+  ]
+}).catch(err => console.error(err))
